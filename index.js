e0704cef442aa07ef754f871aa1c4047b6f06faf
Leorio21 <Ptitlue@gmail.com>
2024-01-13 17:38:13 +0100

    correction formatage code

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 import { prompt } from "./prompt.js";
 
 const guessTheNumber = () => {
-
-  const MIN_NUMBER = 0
+  const MIN_NUMBER = 0;
   const MAX_NUMBER = 1000;
-
   let replay = "O";
 
-
-    const isValidNumber = (message) => {
+  const isValidNumber = (message) => {
     const input = Number(prompt(message));
     if (!Number.isNaN(input) && input >= MIN_NUMBER && input <= MAX_NUMBER) {
       return input;
@@ -18,15 +15,12 @@ const guessTheNumber = () => {
 
   const play = () => {
     const targetNumber = Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER)) + MIN_NUMBER;
-
     const nbTry = game(targetNumber);
-
-    console.log(`Gagné en ${nbTry} essai`);
+    console.log(`Gagné en ${nbTry} tentative(s)`);
   }
 
   const game = (targetNumber) => {
     const palyerInput = isValidNumber(`Saisir un nombre entre ${MIN_NUMBER} et ${MAX_NUMBER} :`);
-
     if (targetNumber === palyerInput) {
       return 1;
     }
@@ -44,7 +38,6 @@ const guessTheNumber = () => {
     replay = prompt("Voulez-vous rejouer (O/N) ?").toUpperCase();
   }
   console.log("A bientôt");
-
 };
 
 guessTheNumber();
